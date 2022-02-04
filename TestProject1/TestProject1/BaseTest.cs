6f638c6480956a6d6676d8373a5f74ceff7ea174
cursedone666 <52870739+cursedone666@users.noreply.github.com>
2022-02-04 15:33:40 +0200

    Update BaseTest.cs

--- TestProject1/TestProject1/BaseTest.cs
+++ TestProject1/TestProject1/BaseTest.cs
@@ -10,13 +10,13 @@ namespace TestProject1
 {
     public class BaseTest : IDisposable
     {
-        public IWebDriver StartDriverWithURL(string url)
+        public void StartDriverWithURL(string url)
         {
             DriverHolder.driver = new ChromeDriver();
             DriverHolder.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             DriverHolder.driver.Manage().Window.Maximize();
             DriverHolder.driver.Navigate().GoToUrl(url);
-            return DriverHolder.driver;
+            
         }
         public void Dispose()
         {
